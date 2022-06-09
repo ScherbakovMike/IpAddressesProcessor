f7f33a1408e4096af59348889c23b19f112dac45
Mike Scherbakov <62207753+ScherbakovMike@users.noreply.github.com>
2022-06-09 21:05:18 +0300

    Added functional

--- src/main/java/IpReader.java
+++ src/main/java/IpReader.java
@@ -1,61 +1,81 @@
-import com.sun.nio.file.ExtendedOpenOption;
-
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
-
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import static java.nio.file.StandardOpenOption.READ;
-
 @Slf4j
-public class IpReader extends RecursiveTask<List<String>> {
+public class IpReader extends RecursiveTask<Long> {
     private final Path file;
-    private final int start;
-    private final int end;
+    private final long start;
+    private final long end;
 
-    private final static long THRESHOLD = 10_000_000L;
+    private static int arrayLenDivider = 4;
+    private static int arrayLen = Integer.MAX_VALUE / arrayLenDivider;
+    private static int arraysCount = 8;
+    private final static byte[][] byteArray = new byte[arrayLen][arraysCount];
+    private final static long THRESHOLD = 100_000L;
     private final static String delimiter = System.lineSeparator();
 
     private final static long seekBufferSize = ("255.255.255.255" + delimiter + "255.255.255.255").getBytes().length;
 
-    public IpReader(Path file, int start, int end) {
+    public IpReader (Path file, long start, long end) {
         this.file = file;
         this.start = start;
         this.end = end;
     }
 
     @Override
-    protected List<String> compute() {
-        int size = end - start + 1;
-        if ((end - start + 1) < THRESHOLD) {
-            return Arrays.asList(new String(readBytes(file, start, end).array()).split(delimiter));
+    protected Long compute () {
+        long size = end - start + 1;
+        if (size < THRESHOLD) {
+            try {
+                var list = new ArrayList<>(Arrays.asList(new String(readBytes(file,
+                    start,
+                    end
+                ).array()).split(delimiter)));
+                setIpToArray(list);
+                return (long) list.size();
+            } catch (Exception e) {
+                log.error(this.getClass().getSimpleName(), e);
+                return 0L;
+            }
         }
 
-        ByteBuffer buffer = readBytes(file, start, end);
-        long delimiterPosition = findDelimiterPosition();
-        if (delimiterPosition<0) {
-            return Arrays.asList(new String(buffer.array()).split(delimiter));
+        long delimiterPosition = findDelimiterPosition(file, start, end);
+        if (delimiterPosition < 0) {
+            try {
+                var list = new ArrayList<>(Arrays.asList(new String(readBytes(file,
+                    start,
+                    end
+                ).array()).split(delimiter)));
+                setIpToArray(list);
+                return (long) list.size();
+            } catch (Exception e) {
+                log.error(this.getClass().getSimpleName(), e);
+                return 0L;
+            }
         } else {
             var leftTask = new IpReader(file, start, delimiterPosition);
-            var rightTask = new IpReader(file, delimiterPosition+1, end);
+            var rightTask = new IpReader(file, delimiterPosition + 1, end);
             leftTask.fork();
             var rightResult = rightTask.compute();
             var leftResult = leftTask.join();
-            leftResult.addAll(rightResult);
-            return leftResult;
+            return leftResult + rightResult;
         }
     }
 
     @SneakyThrows
-    private ByteBuffer readBytes(Path path, long start, long end) {
+    private ByteBuffer readBytes (Path path, long start, long end) {
         long size = end - start + 1;
 
-        try (var byteChannel = Files.newByteChannel(path, READ)) {
+        try (var reader = new RandomAccessFile(path.toFile(), "r"); var byteChannel = reader.getChannel()) {
             byteChannel.position(start);
             var buffer = ByteBuffer.allocate((int) size);
             byteChannel.read(buffer);
@@ -64,32 +84,49 @@ public class IpReader extends RecursiveTask<List<String>> {
             log.error(getClass().getSimpleName(), e);
             return null;
         }
-//
-//        int size = end - start + 1;
-//        int bufferSize = (size / 4096 + 1) * 4096;
-//        try (var inputStream = Files.newInputStream(file, ExtendedOpenOption.DIRECT)) {
-//            byte[] buffer = new byte[bufferSize];
-//            inputStream.readNBytes(buffer, start, bufferSize);
-//            return buffer;
-//        } catch (Exception e) {
-//            log.error(getClass().getSimpleName(), e);
-//            return new byte[0];
-//        }
     }
 
-    private long findDelimiterPosition(ByteBuffer buffer) {
-        long bufferSize = buffer.capacity();
-        long middle = bufferSize / 2;
-        long testBufferSize = Math.min(seekBufferSize, bufferSize);
-        long testBufferStart = middle - testBufferSize / 2;
-        long testBufferEnd = middle + testBufferSize / 2;
-        long seekBufferSize = testBufferEnd - testBufferSize + 1;
-        byte[] seekBuffer = new byte[(int) seekBufferSize];
-        buffer.get(seekBuffer, (int) testBufferStart, (int) seekBufferSize);
-        int delimiterPosition = (new String(seekBuffer)).indexOf(delimiter);
-        if (delimiterPosition >= 0) {
-            delimiterPosition += testBufferStart;
+    private long findDelimiterPosition (Path file, long start, long end) {
+        var delimiterPosition = -1L;
+        long middle = (start + end) / 2;
+        long left = middle - seekBufferSize / 2;
+        long right = middle + seekBufferSize / 2;
+        var buffer = readBytes(file, left, right);
+        var string = new String(buffer.array());
+        var pos = string.indexOf(delimiter);
+        if (pos >= 0) {
+            delimiterPosition = left + pos;
         }
         return delimiterPosition;
+    }
+
+    private long parseIp (String address) {
+        long result = 0L;
+        for (String part : address.split(Pattern.quote("."))) {
+            result = result << 8;
+            result |= Integer.parseInt(part);
+        }
+        if(result<0L) System.out.println(address);
+        return result;
+    }
+
+    private void setIpToArray (List<String> list) {
+        for(int i = 0; i< list.size();i++){
+            var address = list.get(i);
+            long ip = parseIp(address);
+            int arrayNumber = (int) (ip / arrayLen);
+            int arrayPosition = (int) (ip % arrayLen);
+            byteArray[arrayPosition][arrayNumber] = 1;
+        }
+    }
+
+    public static long countOfUnique () {
+        long sum = 0L;
+        for (int i = 0; i < arrayLen; i++) {
+            for (int j = 0; j < arraysCount; j++) {
+                sum += byteArray[i][j];
+            }
+        }
+        return sum;
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,91 +1,32 @@
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
+import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.Objects;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Predicate;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+import java.util.concurrent.ForkJoinPool;
 import lombok.SneakyThrows;
-
-import static java.nio.file.StandardOpenOption.*;
 
 class Solution {
 
-    private static Random random = new Random();
-    private static String lineSeparator = System.lineSeparator();
+    private static final Random random = new Random();
+    private static final String lineSeparator = System.lineSeparator();
 
     @SneakyThrows
     public static void main (String[] args) {
-        //generateTestFile();
 
-        try(var byteChannel = Files.newByteChannel(getTestFilePath(), READ)) {
+        generateTestFile();
+        var path = getTestFilePath();
 
+        try (var reader = new RandomAccessFile(path.toFile(), "r"); var byteChannel = reader.getChannel()) {
+            var ipReader = new IpReader(path, 0, Files.size(path) - 1);
+            new ForkJoinPool().invoke(ipReader);
+            System.out.println(IpReader.countOfUnique());
         }
-//        try (var reader = Files.newBufferedReader(getTestFilePath())) {
-//            //System.out.println(reader.lines().skip(1000_0000).limit(1000_000).distinct().count());
-//            String line = null;
-//            AtomicLong counter = new AtomicLong();
-//            Stream<String> stream = breakStream(
-//                Stream.generate(() -> new IntStream().map(operand -> )lineGenerator(reader, counter.getAndIncrement())),
-//                Objects::isNull);
-////            Stream<String> stream = Stream.generate(() -> lineGenerator(reader, counter.getAndIncrement()))
-////                                          .takeWhile(Objects::nonNull)
-////                                          .distinct();
-//            System.out.println(stream.distinct().count());
-//        }
-//
-//        //var count = Files.lines(getTestFilePath()).distinct().count();
-//        //System.out.println(count);
-//        //        Path testFile = Path.of(Objects.requireNonNull(Solution.class.getResource("testFile.txt")).getFile());
-//        //        var pool = new ForkJoinPool();
-//        //        var result = pool.invoke(new IpReader(testFile, 0, (int) Files.size(testFile) - 1));
-//        //        System.out.println(result.parallelStream().unordered().distinct().count());
     }
 
-//    @SneakyThrows
-//    private static String lineGenerator (BufferedReader reader, long counter) {
-//        if ((counter % 1000_000) == 0) {
-//            System.gc();
-//        }
-//        return reader.readLine();
-//    }
-//
-//    public static <T> Stream<T> breakStream(Stream<T> stream, Predicate<T> terminate) {
-//        final Iterator<T> original = stream.iterator();
-//        Iterable<T> iter = () -> new Iterator<T>() {
-//            T t;
-//            boolean hasValue = false;
-//
-//            @Override
-//            public boolean hasNext() {
-//                if (!original.hasNext()) {
-//                    return false;
-//                }
-//                t = original.next();
-//                hasValue = true;
-//                return !terminate.test(t);
-//            }
-//
-//            @Override
-//            public T next() {
-//                if (hasValue) {
-//                    hasValue = false;
-//                    return t;
-//                }
-//                return t;
-//            }
-//        };
-//
-//        return StreamSupport.stream(iter.spliterator(), false);
-//    }
     @SneakyThrows
     private static void generateTestFile () {
         int count = 1000_000;

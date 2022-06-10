f98fb910c3a29ce0d3e8d7cb15f90e809fcec70f
Mike Scherbakov <62207753+ScherbakovMike@users.noreply.github.com>
2022-06-10 10:43:56 +0300

    add

--- src/main/java/IpReader.java
+++ src/main/java/IpReader.java
@@ -1,12 +1,14 @@
+import com.google.common.collect.Sets;
 import java.io.RandomAccessFile;
+import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.RecursiveTask;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,12 +18,24 @@ public class IpReader extends RecursiveTask<Long> {
     private final long start;
     private final long end;
 
+    private final static long THRESHOLD = 100_000;
+    private final static String delimiter = System.lineSeparator();
+
     private static int arrayLenDivider = 4;
     private static int arrayLen = Integer.MAX_VALUE / arrayLenDivider;
     private static int arraysCount = 8;
-    private final static byte[][] byteArray = new byte[arrayLen][arraysCount];
-    private final static long THRESHOLD = 100_000L;
-    private final static String delimiter = System.lineSeparator();
+    private final static byte[][] byteArray;
+    static {
+        System.out.printf("Memory allocation has been started.\n");
+        var startTime = Instant.now();
+        byteArray = new byte[arrayLen][arraysCount];
+        var endTime = Instant.now();
+        System.out.printf("Memory has been allocated in %d seconds.\n",
+            (endTime.getEpochSecond() - startTime.getEpochSecond())
+        );
+    }
+//
+//    private static final Set<Long> set = Sets.newConcurrentHashSet();
 
     private final static long seekBufferSize = ("255.255.255.255" + delimiter + "255.255.255.255").getBytes().length;
 
@@ -36,12 +50,9 @@ public class IpReader extends RecursiveTask<Long> {
         long size = end - start + 1;
         if (size < THRESHOLD) {
             try {
-                var list = new ArrayList<>(Arrays.asList(new String(readBytes(file,
-                    start,
-                    end
-                ).array()).split(delimiter)));
+                var list = new String(readBytes(file, start, end).array()).split(delimiter);
                 setIpToArray(list);
-                return (long) list.size();
+                return (long) list.length;
             } catch (Exception e) {
                 log.error(this.getClass().getSimpleName(), e);
                 return 0L;
@@ -51,12 +62,9 @@ public class IpReader extends RecursiveTask<Long> {
         long delimiterPosition = findDelimiterPosition(file, start, end);
         if (delimiterPosition < 0) {
             try {
-                var list = new ArrayList<>(Arrays.asList(new String(readBytes(file,
-                    start,
-                    end
-                ).array()).split(delimiter)));
+                var list = new String(readBytes(file, start, end).array()).split(delimiter);
                 setIpToArray(list);
-                return (long) list.size();
+                return (long) list.length;
             } catch (Exception e) {
                 log.error(this.getClass().getSimpleName(), e);
                 return 0L;
@@ -100,20 +108,30 @@ public class IpReader extends RecursiveTask<Long> {
         return delimiterPosition;
     }
 
-    private long parseIp (String address) {
+    public static long parseIpShift (String address) {
         long result = 0L;
         for (String part : address.split(Pattern.quote("."))) {
             result = result << 8;
             result |= Integer.parseInt(part);
         }
-        if(result<0L) System.out.println(address);
+        if (result < 0L) System.out.println(address);
         return result;
     }
 
-    private void setIpToArray (List<String> list) {
-        for(int i = 0; i< list.size();i++){
-            var address = list.get(i);
-            long ip = parseIp(address);
+//    @SneakyThrows
+//    public static long parseIpInetAddr (String address) {
+//        return
+//            (ByteBuffer.allocate(Integer.BYTES)
+//                      .put(InetAddress.getByName(address).getAddress())
+//                      .getInt(0)
+//                +Integer.MAX_VALUE);
+//    }
+
+    private void setIpToArray (String[] list) {
+        for (String address : list) {
+            long ip = parseIpShift(address);
+//            long ip = parseIpInetAddr(address);
+//            set.add(ip);
             int arrayNumber = (int) (ip / arrayLen);
             int arrayPosition = (int) (ip % arrayLen);
             byteArray[arrayPosition][arrayNumber] = 1;
@@ -121,12 +139,19 @@ public class IpReader extends RecursiveTask<Long> {
     }
 
     public static long countOfUnique () {
+        System.out.printf("Counting of result has been started.\n");
+        var startTime = Instant.now();
         long sum = 0L;
         for (int i = 0; i < arrayLen; i++) {
             for (int j = 0; j < arraysCount; j++) {
                 sum += byteArray[i][j];
             }
         }
+        //long sum = set.size();
+        var endTime = Instant.now();
+        System.out.printf("Counting of result has been finished in %d seconds.\n",
+            (endTime.getEpochSecond() - startTime.getEpochSecond())
+        );
         return sum;
     }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -5,6 +7,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
 import lombok.SneakyThrows;
@@ -19,25 +23,34 @@ class Solution {
 
         generateTestFile();
         var path = getTestFilePath();
-
+        var startTime  = Instant.now();
         try (var reader = new RandomAccessFile(path.toFile(), "r"); var byteChannel = reader.getChannel()) {
             var ipReader = new IpReader(path, 0, Files.size(path) - 1);
+            System.out.printf("%s: Reading process has been started.\n", new Date());
             new ForkJoinPool().invoke(ipReader);
-            System.out.println(IpReader.countOfUnique());
+            System.out.printf("%s: Reading process has been finished.\n", new Date());
+            System.out.printf("Result: %d unique IPs.\n",IpReader.countOfUnique());
         }
+        var endTime = Instant.now();
+        System.out.printf("File scanned  in %d seconds.\n",
+            (endTime.getEpochSecond()-startTime.getEpochSecond()));
     }
 
     @SneakyThrows
     private static void generateTestFile () {
+        System.out.printf("Test file generating has been started.\n");
         int count = 1000_000;
         Path tesFileResource = getTestFilePath();
-
-        try (var outputStream = Files.newOutputStream(tesFileResource, CREATE, APPEND)) {
+        var startTime  = Instant.now();
+        try (var outputStream = Files.newOutputStream(tesFileResource, CREATE, APPEND);
+             var bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream))) {
             for (int i = 0; i < count; i++) {
-                outputStream.write(getRandomIp());
-                outputStream.write(lineSeparator.getBytes());
+                bufferedWriter.write(getRandomIp()+lineSeparator);
             }
         }
+        var endTime = Instant.now();
+        System.out.printf("Test file generated with %d IPs in %d seconds.\n",
+            count, (endTime.getEpochSecond()-startTime.getEpochSecond()));
     }
 
     private static Path getTestFilePath () throws URISyntaxException {
@@ -45,7 +58,12 @@ class Solution {
         return resourcesPath.resolve(Paths.get("testFile.txt"));
     }
 
-    private static byte[] getRandomIp () {
-        return (random.nextInt(255) + "." + random.nextInt(255) + "." + random.nextInt(255) + "." + random.nextInt(255)).getBytes();
+    private static String getRandomIp () {
+        return (random.nextInt(255)
+            + "."
+            + random.nextInt(255)
+            + "." +
+            random.nextInt(255)
+            + "." + random.nextInt(255));
     }
 }
